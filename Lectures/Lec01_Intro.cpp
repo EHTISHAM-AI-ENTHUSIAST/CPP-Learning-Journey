585ef836e4f0130e3df49bb0d0e031bedf0eedc7
Ehtisham Ashraf <kingehtisham0@gmail.com>
2026-02-17 10:00:00 +0500

    feat: add Lec01_Intro.cpp (Lectures)

---
+++ Lectures/Lec01_Intro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  //preprocessor directive to include the iostream library
+using namespace std; // using the standard namespace library
+int main()           // main function - execution starts here
+
+{
+    cout << "virtual university of pakistan" << endl;
+    cout << "computer science department" << endl;
+    cout << "C++ programming language" << endl;
+
+    return 0; // indicate that the program ended successfully
+}
