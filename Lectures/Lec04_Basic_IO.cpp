87ba09c5e5f4b151856fb771f8d9ef5e9c8c1757
Ehtisham Ashraf <kingehtisham0@gmail.com>
2026-02-17 10:00:00 +0500

    feat: add Lec04_Basic_IO.cpp (Lectures)

---
+++ Lectures/Lec04_Basic_IO.cpp
@@ -0,0 +1,18 @@
+#include <iostream>  
+using namespace std; 
+int main()           
+
+{
+    int a;
+    int b;
+
+    a = 10;
+
+    cout << "Enter the value of b: "; 
+    cin >> b;
+
+    cout << "the value of a = " << a << endl;
+    cout << "the value of b = " << b << endl;
+    
+    return 0; 
+}
