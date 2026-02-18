b24f0c5fd9601af3f0faadf4af6cd7d5084c8807
Ehtisham Ashraf <kingehtisham0@gmail.com>
2026-02-18 19:13:48 +0500

    feat: add Lec07_Simple_Table.cpp (Lectures)

---
+++ Lectures/Lec07_Simple_Table.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+  int counter;
+
+    for (counter = 1; counter <= 10; counter++) 
+        cout << "3 x " << counter << " = " << 3 * counter << "\n";
+    return 0;
+}
